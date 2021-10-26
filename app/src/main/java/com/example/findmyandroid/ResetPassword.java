d8f1745133da0ef0fe0d0e68ffe17516a241fc7a
Wei Zhong Tee <weizhong.tee@jamf.com>
2021-10-26 16:24:12 -0500

    Add files

---
+++ app/src/main/java/com/example/findmyandroid/ResetPassword.java
@@ -0,0 +1,56 @@
+package com.example.findmyandroid;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.findmyandroid.databinding.FragmentResetPasswordBinding;
+import com.example.findmyandroid.databinding.FragmentUserRegistrationBinding;
+
+public class ResetPassword extends Fragment {
+
+    private FragmentResetPasswordBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentResetPasswordBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        binding.buttonConfirm.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavHostFragment.findNavController(ResetPassword.this)
+//                        .navigate(R.id.action_reset_password_to_login);
+//            }
+//        });
+
+        binding.buttonBackToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ResetPassword.this)
+                        .navigate(R.id.action_reset_password_to_login);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
